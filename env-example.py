546a4763a7ab29c25936d7382be90025f03dc354
elias4k <40409522+elias4k@users.noreply.github.com>
2022-02-17 11:47:30 -0300

    Add Token Provisorio de Telegram en env-example.py

--- env-example.py
+++ env-example.py
@@ -1,6 +1,7 @@
 def env(word):
     lista = {
-        "telegram-token": "[Token de telegram obtenido de @BotFather]", 
+        # "telegram-token": "[Token de telegram obtenido de @BotFather]", 
+        "telegram-token": "2102316937:AAGmAMknKsYazUlQSgyY7ipTwcWhFvqe-iI", # Token Provisorio @E4K01_bot o https://t.me/E4K01_bot
         "cod-vacuna": "[Codigo-verificacion-vacuna]",
         "mongo_uri": "[ConectionString]",
         "path-chrome": '[Path del ejecutable chromedriver]',
